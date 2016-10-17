0b58e30904a8019b20be45b5381e680706b97d8c
LilianaCanPor <liliana.canpor@gmail.com>
2016-10-17 16:55:44 -0500

    Otra opcion, ej2-No es perfecta

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -14,10 +14,14 @@ function validateForm(){
 
 	function validatePass(){
 		var pass = document.getElementById('input-password').value;
-		if((pass.length < 6) || (pass  === 'password') || (pass === '123456') || (pass ==='0987654')){
+		if((pass.length < 6) /*|| (pass  === 'password') || (pass === '123456') || (pass ==='0987654')*/){
 			alert('Elija otra contraseña. Minimo debe contener 6 caractes');
 			return false;
 		}
+		if( !(/^\d{6}$/.test(pass)) ) {
+			alert('Elija otra contraseña. No puede contener numeros consecutivos');
+ 			return false;
+}
 
 	}
 
