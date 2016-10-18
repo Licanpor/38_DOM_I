7502226a07c8b4213b961bc8332ff07c19128e96
LilianaCanPor <liliana.canpor@gmail.com>
2016-10-18 16:09:56 -0500

    Mejorado

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,28 +1,27 @@
 function validateForm(){
 	var name = document.getElementById('name').value;
 	var lastname = document.getElementById('lastname').value;
-	var patron = /^[A-Z][a-z]*$/;
-	validatePass();
-	if (!patron.test (name)) {
-		alert('Ingrese su nombre con la primera letra mayúscula');
-		return false;
+	var pass = document.getElementById('input-password').value;
+	var mail = document.getElementById('input-email').value;
+	var reName = /^[A-Z][a-z]*$/;
+	var rePass = /^\d{6}$/;
+	var errores = true;
+	var mensaje = " ";
+	
+	if (!reName.test (name)) {
+		mensaje += 'Ingrese su nombre con la primera letra mayúscula ';
+		errores = false;
 	}	
-	if (!patron.test (lastname)) {
-		alert('Ingrese su apellido con la primera letra mayúscula');
-		return false;	
+	if (!reName.test (lastname)) {
+		mensaje += 'Ingrese su apellido con la primera letra mayúscula ';
+		errores = false;	
 	}
-
-	function validatePass(){
-		var pass = document.getElementById('input-password').value;
-		if((pass.length < 6) /*|| (pass  === 'password') || (pass === '123456') || (pass ==='0987654')*/){
-			alert('Elija otra contraseña. Minimo debe contener 6 caractes');
-			return false;
-		}
-		if( !(/^\d{6}$/.test(pass)) ) {
-			alert('Elija otra contraseña. No puede contener numeros consecutivos');
- 			return false;
-}
-
+	if((pass.length < 6) || (pass  === 'password') || (!rePass.test(pass))){
+		mensaje += 'Elija otra contraseña. Minimo debe contener 6 caractes ';
+		errores = false;
 	}
-
+	if(!errores){
+		alert(mensaje);
+	}
+return errores;
 }
