ff1e238f013d90f01d1fa27992ff68084a856d82
LilianaCanPor <liliana.canpor@gmail.com>
2016-10-26 11:45:02 -0500

    Agregue comentarios

--- traduccion-formulario/js/main.js
+++ traduccion-formulario/js/main.js
@@ -1,8 +1,14 @@
 var elFormulario = document.getElementById('form-signin-heading');
+//este metodo permite obtener el elemnto por medio de si id y guardarle en la variable.
 elFormulario.innerHTML = 'Por favor inicia sesión';
+/*este metodo agregara al elemento en el html esta cadena de texto es decir:
+<h2 id="form-signing-heading>"
+	Porfavor inicia sesion
+</h2>*/
 
 var elEmail = document.getElementById('inputEmail');
 elEmail.setAttribute('placeholder', 'Correo electrónico');
+//setAttribute agregara un atributo placeholder al elemento guardado en elEmail(input) con su valor correspondiente.
 
 var elPass = document.getElementById('inputPassword');
 elPass.setAttribute('placeholder', 'Contraseña');
@@ -10,5 +16,6 @@ elPass.setAttribute('placeholder', 'Contraseña');
 // $('#inputPassword').attr('placeholder','Contraseña'); --->Segun cambia el nombre de un placeholder. Pero no resulto.
 
 var recordarme = document.getElementsByTagName('span')[0].innerHTML = 'Recordar datos';//La posicion suplira el valor del seleccionado. creo**
-
+/*ontiene el elemento por medio del nombre de su etiqueta. El resultado es un arreglo 
+mediante el index le indicas cual de todas esas etiquetas es la que buscas.*/
 var boton = document.getElementsByTagName('button')[0].innerHTML = 'Iniciar Sesión';
